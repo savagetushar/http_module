84d5ba8dabc93bae13e98f9bee3ac5faed970688
savagetushar <pandeytushar287@gmail.com>
2024-06-15 04:20:27 +0530

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const http=require('http');
 
-const server=http.createServer((req,res)=>{
-    if(req.url == '/'){
-        res.write('<h1> Hello,Node js!</h1>')
-    }
+// const server=http.createServer((req,res)=>{
+//     if(req.url == '/'){
+//         res.write('<h1> Hello,Node js!</h1>')
+//     }
+    
+//     res.end();
+    
+    
+// });
 
-    res.end();
+// server.listen(5000);
+// console.log('The server running on port number 5000');
+
+const port=3020;
+const hostname='localhost';
+const server=http.createServer((req,res)=>{
+
+    res.end('Node server created by tushar');
 
 
 });
 
-server.listen(5000);
-console.log('The server running on port number 5000');
+server.listen(port,()=>{
+
+    console.log('server running at the ${hostname}:${port}');
+
+});
+
+
+
+
