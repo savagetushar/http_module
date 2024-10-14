e5801b3be02dd0d9d5467c540de3277bdef11ce7
Tushar kumar <98999845+savagetushar@users.noreply.github.com>
2024-10-14 23:42:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-// const http=require('http');
+ const http=require('http');
 
-// // const server=http.createServer((req,res)=>{
-// //     if(req.url == '/'){
-// //         res.write('<h1> Hello,Node js!</h1>')
-// //     }
+ const server=http.createServer((req,res)=>{
+     if(req.url == '/'){
+        res.write('<h1> Hello,Node js!</h1>')
+     }
     
-// //     res.end();
+     res.end();
     
     
-// // });
+ });
 
-// // server.listen(5000);
-// // console.log('The server running on port number 5000');
+ server.listen(5000);
+ console.log('The server running on port number 5000');
 
-// const port=3022;
-// const hostname='localhost';
-// const server=http.createServer((req,res)=>{
+ const port=3022;
+ const hostname='localhost';
+ const server=http.createServer((req,res)=>{
 
-//     res.statusCode=500;
-//     res.setHeader('content-type','application json');
-
-
-//     res.end(JSON.stringify({error:"server error"}));
+     res.statusCode=500;
+     res.setHeader('content-type','application json');
 
 
+     res.end(JSON.stringify({error:"server error"}));
 
-// });
 
-// server.listen(port,()=>{
 
-//     console.log('server running at the ${hostname}:${port}');
+ });
 
-// });
+ server.listen(port,()=>{
+
+     console.log('server running at the ${hostname}:${port}');
+
+ });
 
 
 
